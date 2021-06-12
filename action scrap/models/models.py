763107b66149b158aaab3956ad002c63e51896e2
Odoo-Fitex <68330713+Odoo-Fitex@users.noreply.github.com>
2021-06-12 12:31:36 +0200

    Update models.py

--- action scrap/models/models.py
+++ action scrap/models/models.py
@@ -5,7 +5,7 @@ from odoo.tools import float_compare
 
 class StockScrap(models.Model):
     _inherit = 'stock.scrap'
-
+ 
     location_id = fields.Many2one(
         'stock.location', 'Source Location',
         domain="[('usage', 'in', ['internal', 'transit']), ('company_id', 'in', [company_id, False])]",
